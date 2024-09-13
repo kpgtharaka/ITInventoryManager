1128592ff42b2ea644980e5113b0ba149a882ace
K.P.Gayan Tharaka <kpgtharaka@gmail.com>
2024-09-13 22:26:41 +0530

    added emp_no to Model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
-    protected $fillable = ['first_name','last_name','email','phone','address'];
+    protected $fillable = ['emp_no','first_name','last_name','email','phone','address'];
 }
