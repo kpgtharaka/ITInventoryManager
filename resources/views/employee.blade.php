d55adcea0e272b3393d8369b4a6367e45720cec8
K.P.Gayan Tharaka <kpgtharaka@gmail.com>
2024-09-13 22:40:04 +0530

    Add Route; add blade template for employee
    update employee controller index()

---
+++ resources/views/employee.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Employee</title>
+</head>
+<body>
+    @foreach ($employees as $employee)
+        {{ $employee->emp_no }} {{ $employee->first_name }} {{ $employee->last_name }} {{ $employee->email}} {{ $employee->phone}} {{ $employee->address}} <br>
+    @endforeach
+</body>
+</html>
