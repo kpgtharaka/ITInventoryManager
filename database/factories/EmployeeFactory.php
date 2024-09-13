360f6cdc905704ccf51ebf13a3a7389c8803780d
K.P.Gayan Tharaka <kpgtharaka@gmail.com>
2024-09-13 22:27:16 +0530

    Create Employee Factory and Seed data

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -17,7 +17,12 @@ class EmployeeFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'emp_no'=>fake()->numerify('EMP###'),
+            'first_name'=>fake()->firstName(),
+            'last_name'=>fake()->lastName(),
+            'email'=>fake()->email(),
+            'phone'=>fake()->phoneNumber(),
+            'address'=>fake()->address()
         ];
     }
 }
