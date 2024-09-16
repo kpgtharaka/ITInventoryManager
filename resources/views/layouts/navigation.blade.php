0545a5fafcf318c651cd4170a92f2b2cc412e0e7
K.P.Gayan Tharaka <kpgtharaka@gmail.com>
2024-09-16 14:56:47 +0530

    Mobile Responsive fixed with JS
    removed alpine code and rewrite using JS

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
       </a>
     </div>
     <div class="flex lg:hidden">
-      <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+      <button id="open" type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
         <span class="sr-only">Open main menu</span>
         <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -23,7 +23,7 @@
   </nav>
   
   <!-- Mobile menu, show/hide based on menu open state. -->
-  <div class="lg:hidden " role="dialog" aria-modal="true">
+  <div class="lg:hidden " role="dialog" aria-modal="true" id="responsivemenu">
     <!-- Background backdrop, show/hide based on slide-over state. -->
     <div class="fixed inset-0 z-50"></div>
     <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
@@ -32,7 +32,7 @@
           <span class="sr-only">Your Company</span>
           <img class="h-8 w-auto" src="/images/logo.png" alt="">
         </a>
-        <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+        <button id="close" type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
           <span class="sr-only">Close menu</span>
           <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -51,3 +51,14 @@
       </div>
     </div>
   </div>
+  <script>
+    const open = document.getElementById('open');
+    const close = document.getElementById('close');
+
+    open.addEventListener('click', () => {
+      responsivemenu.classList.toggle('hidden');
+    });
+    close.addEventListener('click', () => {
+      responsivemenu.classList.toggle('hidden');
+    });
+  </script>
