8ef89e84a8f5c13d485fa02c604e1578c8e9c4e9
K.P.Gayan Tharaka <kpgtharaka@gmail.com>
2024-09-16 09:01:55 +0530

    Named home route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('employee/create', [EmployeeController::class,'create'])->name('employee.create');
 Route::post('employee/store', [EmployeeController::class,'store'])->name('employee.store');
